02e07859ef713300d440f6e954c27426b12f4f43
kerbo li <kerbo li>
2017-01-12 21:06:51 +0800

    asasdwdadw

--- weather.py
+++ weather.py
@@ -1,13 +1,24 @@
-#-*-coding:gbk -*-
+#-*-coding: GBK -*-
 import urllib2
 import json
 from city import city
 
-# cityname = ram_input('你想查那个城市的天气\n')
 cityname = raw_input('你想查哪个城市的天气？\n')
 citycode = city.get(cityname)
 
 if citycode:
-    url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
-    concent = urllib2.urlopen(url).read()
-    print concent
+	try:		
+		url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
+		concent = urllib2.urlopen(url).read()
+		data = json.loads(concent)
+		result = data['weatherinfo']
+		str_temp = ('%s\n%s ~ %s') % (
+			result['weather'],
+			result['temp1'],
+			result['temp2']
+		)
+		print str_temp
+	except:
+		print '查询失败'
+else:
+	print '没有城市'
